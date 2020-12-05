0f9588b02748e1db4017fc4c4cdc3ec6dc9f337a
greenLadros <67965236+greenLadros@users.noreply.github.com>
2020-12-05 21:52:29 +0200

    add environment

---
+++ Project/Separated/Source/Separated/SeparatedGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SeparatedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEPARATED_API ASeparatedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
